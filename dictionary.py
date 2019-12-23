29645b195376dc26dcce3a307f135534c994fa16
Mukta Girkar <muktagirkar@gmail.com>
2019-12-22 16:07:34 -0800

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+#dictionary is the datatype which stores data in key value pairs
+>>> #can create dict object like this
+... 
+>>> {}
+{}
+>>> type({})
+<class 'dict'>
+>>> #or like this
+... 
+>>> dict()
+{}
+>>> #dictionaries are mutable (you can add n remove items from dict; but dictionary keys are immutable)
+... 
+>>> 
