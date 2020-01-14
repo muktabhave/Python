f27b1e29e76c9b09030c4cc69cf2be050963b76d
Mukta Girkar <muktagirkar@gmail.com>
2020-01-14 00:01:39 -0800

    Add files via upload

---
+++ Looping.py
@@ -0,0 +1,35 @@
+#for loop
+>>> colors=["red", "blue", "black"]
+>>> for i in colors:
+...     print(i)
+... 
+red
+blue
+black
+>>> range(5)
+range(0, 5)
+>>> #this range executes from 0 to 4 (excludes last element)
+>>> for i in range(5):
+...     print(i)
+... 
+0
+1
+2
+3
+4
+>>> 
+>>> for i in range (1,5):
+...     print(i)
+... 
+1
+2
+3
+4
+>>> #we can mention number of steps to jump over in for loop
+... 
+>>> for i in range (1,7,2):
+...     print(i)
+... 
+1
+3
+5
