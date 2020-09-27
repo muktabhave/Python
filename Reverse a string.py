2bea723d3089d7f61464f58bfb40201fbc9d6bd7
Mukta Girkar <muktagirkar@gmail.com>
2020-09-27 15:54:30 -0700

    Update Reverse a string.py

--- Reverse a string.py
+++ Reverse a string.py
@@ -1,3 +1,14 @@
+#Method-1:
+
+def reverse(s): 
+  return s[::-1]
+  
+if(__name__=="__main__"):
+    
+    print(reverse("Geeksforgeeks"))
+
+#Method-2:
+  
 def reverse(s): 
   str = "" 
   for i in s: 
