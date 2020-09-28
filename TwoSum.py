ed6c1c58a20a37bf4c5608e547a41f761bf13775
Mukta Girkar <muktagirkar@gmail.com>
2020-09-27 23:45:56 -0700

    Create TwoSum.py

---
+++ TwoSum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    
+        dict1={}
+    
+        for i in range (len(nums)):
+        
+            dict1[i]= nums[i]
+    
+        for j in dict1.keys():
+        
+            comp=val-dict1[j]
+        
+            if (comp in dict1.values()):
+            
+                return (j, int(dict1[comp]))
