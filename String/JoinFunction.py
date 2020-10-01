ce4023ddac4e0c684d965c431ad611dffc51d03c
Mukta Girkar <muktagirkar@gmail.com>
2020-09-30 21:27:29 -0700

    Create JoinFunction.py

---
+++ String/JoinFunction.py
@@ -0,0 +1,15 @@
+# Sample Input
+
+# this is a string   
+# Sample Output
+
+# this-is-a-string
+
+def split_and_join(line):
+    # write your code here
+    return('-'.join(line.split()))
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
