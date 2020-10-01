46e98f3a3889c6882a2872249a8597db29c00448
Mukta Girkar <muktagirkar@gmail.com>
2020-09-30 21:43:31 -0700

    Create MutateString.py

---
+++ String/MutateString.py
@@ -0,0 +1,17 @@
+# Change 6th character in a string
+
+def mutate_string(string, position, character):
+    
+    l = list(string)
+    l[5] = 'k'
+    string = ''.join(l)
+    return(''.join(string))
+
+#string = string[:5] + "k" + string[6:]
+#print string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
