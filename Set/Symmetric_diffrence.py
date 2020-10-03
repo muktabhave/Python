08936592dfa32bc7143de26f7daf61fcb0362e24
Mukta Girkar <muktagirkar@gmail.com>
2020-10-02 18:34:09 -0700

    Create Symmetric_diffrence.py

---
+++ Set/Symmetric_diffrence.py
@@ -0,0 +1,17 @@
+# find no of students enrolled either in english or in french but not both
+
+
+if (__name__=="__main__"):
+
+    e=int(input())
+    eset= set(input().split())
+    f=int(input())
+    fset=set(input().split())
+
+    l= fset^eset
+    count=0
+
+    for i in l:
+        count=count+1
+    
+    print(count)
