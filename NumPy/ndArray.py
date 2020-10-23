7c9d3b14afa7ad94b8eaee3350181e8eb6aa9355
Mukta Girkar <muktagirkar@gmail.com>
2020-10-22 20:40:57 -0700

    Create ndArray.py

---
+++ NumPy/ndArray.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+arr= np.array([1,2,3],
+              [4,2,5])
+              
+print(type(arr))
+
+ndarray
+
+print (arr.ndim)
+
+2
+
+print (arr.shape)
+
+(2,3)
+
+print(arr.size)
+
+6
