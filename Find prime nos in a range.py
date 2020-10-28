eb69b1f8823f6a841d65d1c295965e3a9e272df1
Mukta Girkar <muktagirkar@gmail.com>
2020-10-27 22:39:26 -0700

    Update Find prime nos in a range.py

--- Find prime nos in a range.py
+++ Find prime nos in a range.py
@@ -1,14 +1,20 @@
 def primenos(lower, upper):
 
-    for num in range(lower, upper + 1):
+    for num in range(lower, upper+1):
    # all prime numbers are greater than 1
         if num > 1:
-            for i in range(2, num):
+            flag= True
+            
+            for i in range(2, (num/2 +1)):
+                
                 if (num % i) == 0:
-                    break
-            else:
+                    
+		            flag= False
+		            break
+            
+            if (flag== True):
                 print(num)
             
 if (__name__=="__main__"):
 
-	print(primenos(1,10))
+	primenos(1,10)
