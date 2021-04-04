9ceb3120421a42c4bfcf39336fc86b7916d8cfab
Mukta Girkar <muktagirkar@gmail.com>
2021-04-03 17:21:32 -0700

    Create PalindromeNumber.py

---
+++ String/PalindromeNumber.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        x= str(x)
+        
+        if(x[::-1]==x):
+            return True
+        else:
+            return False
